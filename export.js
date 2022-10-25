660d1266cc339d9d3740a1f72defbbefb0bde83a
benjamin antonioli <benantonioli@outlook.fr>
2022-10-25 18:39:49 +0200

    separate function

---
+++ export.js
@@ -0,0 +1,6 @@
+const db = new Map();
+const dbUser = new Map();
+
+module.exports =  db;
+module.exports =  dbUser;
+
--- index.js
+++ index.js
@@ -5,27 +5,16 @@ const express = require("express");
 const app = express();
 const port = Number(process.env.PORT);
 const translate = require("./translate_function");
-const db = new Map();
-const dbUser = new Map();
-
 const bcrypt = require("bcrypt");
-const saltRounds = 10;
-
-const someOtherPlaintextPassword = "not_bacon";
+//const db = new Map();
+const db = require('./export')
+//const dbUser = new Map();
+const dbUser = require('./export')
+const search = require('./seachFunction')
+const userAlreadyExist = require('./userAlreadyExistFunction')
+const data = db;
 
 app.use(express.json());
-
-function search(email, originalText) {
-  const translations = db.get(email);
-  if (translations) {
-    const found = translations.find(
-      (element) => element.originalText === originalText
-    );
-    if (found) {
-      return found;
-    }
-  }
-}
 
 app.post("/translate", async (req, res) => {
   const body = {
@@ -33,12 +22,17 @@ app.post("/translate", async (req, res) => {
     language: req.body.language,
     email: req.body.email,
   };
-  const isAlreadyTranslated = search(body.email, body.text);
+
+  const userData = dbUser.get(body.email);
+  const id = userData.uuid;
+
+  const isAlreadyTranslated = search(id, body.text);
   if (isAlreadyTranslated) {
-    console.log(isAlreadyTranslated);
+    console.log("ça passe");
     return res.send({
       originalText: body.text,
       translatedText: isAlreadyTranslated.translation,
+      uuid: id
     });
   }
   const translation = await translate(body.text, body.language);
@@ -48,36 +42,33 @@ app.post("/translate", async (req, res) => {
     translation: translatedText,
     language: body.language,
   };
-  const hasAlreadySavedTranslation = db.get(body.email);
+
+  const hasAlreadySavedTranslation = db.get(id);
   if (hasAlreadySavedTranslation) {
     hasAlreadySavedTranslation.push(savedTranslation);
-    db.set(body.email, hasAlreadySavedTranslation);
+    db.set(id, hasAlreadySavedTranslation);
   } else {
-    db.set(body.email, [savedTranslation]);
+    db.set(id, [savedTranslation]);
   }
+
   return res.send({
     originalText: body.text,
     translatedText: translatedText,
+    uuid: id
   });
 });
 
-function userAlreadyExist(email) {
-  const user = dbUser.get(email);
-  if (user) {
-    return user;
-  }
-}
 app.post("/user", (req, res) => {
   const body = {
     firstName: req.body.firstName,
     lastName: req.body.lastName,
-    email: req.body.email,
+    email: req.body.email.toLowerCase().trim(),
     password: req.body.password,
   };
 
+  const saltRounds = 10;
   const myPlaintextPassword = body.password;
   const hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
-
   const userUUID = uuidv4();
 
   const user = {
@@ -87,6 +78,7 @@ app.post("/user", (req, res) => {
     uuid: userUUID,
     password: hash,
   };
+
   const findUserExist = userAlreadyExist(body.email);
   if (findUserExist) {
     return res.status(400).send({
@@ -95,21 +87,15 @@ app.post("/user", (req, res) => {
   }
 
   dbUser.set(body.email, user);
-  console.log(dbUser);
 
   return res.send({
     firstName: body.firstName,
     lastName: body.lastName,
     email: body.email,
     uuid: userUUID,
-    password: hash,
   });
-  //const isAlreadyRegistered = body.email
 });
 
 app.listen(port, () => {
-  console.log(`app listening well`);
+  console.log(`app listening ${port}`);
 });
-
-
-
